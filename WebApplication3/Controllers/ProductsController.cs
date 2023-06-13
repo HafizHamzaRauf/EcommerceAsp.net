a19fb56ed9ea198cdd6f18e4e05f04ed55d4436f
hamza <hamazrauf@gmail.com>
2023-06-13 14:35:42 +0500

    dynamimc

--- WebApplication3/Controllers/ProductsController.cs
+++ WebApplication3/Controllers/ProductsController.cs
@@ -10,14 +10,9 @@ namespace WebApplication3.Controllers
 {
     public class ProductsController : Controller
     {
-        private readonly EcommerceContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly EcommerceContext _context = new EcommerceContext();
 
-        public ProductsController(EcommerceContext context, IWebHostEnvironment webHostEnvironment)
-        {
-            _context = context;
-            _webHostEnvironment = webHostEnvironment;
-        }
+      
 
         public IActionResult Index()
         {
@@ -40,39 +35,44 @@ namespace WebApplication3.Controllers
         [HttpPost]
         public IActionResult Create(string title, string category, string description, decimal price, IFormFile image)
         {
-            Console.WriteLine("here ");
-                string imageUrl = null;
-            return RedirectToAction("Index", "Home"); // Redirect to the home page or any other desired action
+            string imageUrl = null;
+            string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "productImages");
+            string uniqueFileName = Guid.NewGuid().ToString() + ".jpg";
+            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+            imageUrl = "/productImages/" + uniqueFileName; // Save the URL in the database
 
-            // Process the uploaded image
-            if (image!= null && image.Length > 0)
+            try
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
                 {
-                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "productImages");
-                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    imageUrl = "/productImages/" + uniqueFileName; // Save the URL in the database
+                    image.CopyTo(fileStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception or log the error message
+                Console.WriteLine($"Error copying image file: {ex.Message}");
+                // You can choose to return an error view or redirect to an error page
+                return RedirectToAction("AddProduct", "Profile"); // Redirect to the home page or any other desired action
 
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        image.CopyTo(fileStream);
-                    }
-                
-
-                var product = new Product
-                {
-                    Title = title,
-                    Category = category,
-                    Description = description,
-                    Price = price,
-                    ImageUrl = imageUrl
-                };
-
-                _context.Products.Add(product);
-                _context.SaveChanges();
-                return RedirectToAction("Index", "Home"); // Redirect to the home page or any other desired action
             }
 
-            return View(); // Return the view with the invalid form
+            var product = new Product
+            {
+                Title = title,
+                Category = category,
+                Description = description,
+                Price = price,
+                ImageUrl = imageUrl
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home"); // Redirect to the home page or any other desired action
         }
+
+
+
     }
 }
