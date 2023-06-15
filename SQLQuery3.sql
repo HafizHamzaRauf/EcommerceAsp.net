f35ed7a24a6fa7f41fbc27a800938729862941a4
hamza <hamazrauf@gmail.com>
2023-06-15 22:54:01 +0500

    added tag helper

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,11 +1,12 @@
-﻿INSERT INTO Product (Title, Category, Price, Description, ImageUrl)
+﻿INSERT INTO [dbo].[Orders] ([UserId], [ProductId], [Payment], [Status], [Quantity])
 VALUES
-    ('Product 1', 'Category 1', 10.99, 'Description 1', '~/productImages/image1.jpg'),
-    ('Product 2', 'Category 2', 15.99, 'Description 2', '~/productImages/Image2.jpg'),
-    ('Product 3', 'Category 3', 19.99, 'Description 3', '~/productImages/Image3.jpg'),
-    ('Product 4', 'Category 4', 12.99, 'Description 4', '~/productImages/Image4.jpg'),
-    ('Product 5', 'Category 5', 8.99, 'Description 5', '~/productImages/Image5.jpg'),
-    ('Product 6', 'Category 6', 16.99, 'Description 6', '~/productImages/Image6.jpg'),
-    ('Product 7', 'Category 7', 14.99, 'Description 7', '~/productImages/Image7.jpg'),
-    ('Product 8', 'Category 8', 9.99, 'Description 8', '~/productImages/Image8.jpg'),
-    ('Product 9', 'Category 9', 11.99, 'Description 9', '~/productImages/Image9.jpg');
+    (1, 1, 'Cash on Delivery', 'Pending', 1),
+    (2, 2, 'Credit Card', 'Delivered', 2),
+    (3, 3, 'Cash on Delivery', 'Pending', 3),
+    (4, 4, 'Credit Card', 'Delivered', 4),
+    (5, 5, 'Cash on Delivery', 'Pending', 5),
+    (1, 2, 'Credit Card', 'Delivered', 6),
+    (2, 3, 'Cash on Delivery', 'Pending', 7),
+    (3, 4, 'Credit Card', 'Delivered', 8),
+    (4, 5, 'Cash on Delivery', 'Pending', 9),
+    (5, 1, 'Credit Card', 'Delivered', 10);
