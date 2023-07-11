7734d4b2a707cf84b917ace473c1130030045a98
hamza <hamazrauf@gmail.com>
2023-07-12 01:54:53 +0500

    i have added the filter and search functionality

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -9,10 +9,50 @@ namespace WebApplication3.Controllers
 
         public IActionResult Index()
         {
+
             var data = ctx.Products.ToList();
             return View(data);
         }
+        public IActionResult Filter(string categories)
+        {
 
-       
+            
+            var data = ctx.Products.ToList();
+
+            if (!string.IsNullOrEmpty(categories))
+            {
+                // Filter the products based on categories
+                data = data.Where(p => p.Category.Equals(categories)).ToList();
+
+            }
+
+            return View(data);
+        }
+        public IActionResult PriceFilter(string price)
+        {
+            var data = ctx.Products.ToList();
+
+            if (!string.IsNullOrEmpty(price) )
+            {
+                var value = decimal.Parse(price);
+                // Filter the products based on price
+                data = data.Where(p => p.Price < value).ToList();
+            }
+
+            return View(data);
+        }
+        public IActionResult Search(string search)
+        {
+            var data = ctx.Products.ToList();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+              
+                data = data.Where(p => p.Description.Contains(search)).ToList();
+            }
+
+            return View(data);
+        }
+
     }
 }
